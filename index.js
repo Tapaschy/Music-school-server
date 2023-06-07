8f6377ac6855cbdd0bb1731d25e7ad0f82ce6b29
Tapas chowdhury <tapaschy12@gmail.com>
2023-06-07 23:57:14 +0600

    minor change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y4h6mjt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +26,8 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +35,11 @@ async function run() {
     const usersCollection = client.db("musicfairy").collection("users");
 
 // user data
+app.get('/users', async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+  });
+
 app.post('/users', async (req, res) => {
     const user = req.body;
     const query = { email: user.email }
@@ -45,6 +52,22 @@ app.post('/users', async (req, res) => {
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
+//   change user role
+app.patch('/users/role/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const { role } = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: role
+      },
+    };
+
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+
+  })
 
 
     // Send a ping to confirm a successful connection
