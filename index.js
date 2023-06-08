89a7452c4f18c5aa5516b8e40e939cd9d24d727b
Tapas chowdhury <tapaschy12@gmail.com>
2023-06-08 10:54:51 +0600

    admin role

--- index.js
+++ index.js
@@ -52,6 +52,23 @@ app.post('/users', async (req, res) => {
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
+// get user role
+app.get('/users/role/:email', async (req, res) => {
+    const email = req.params.email;
+
+    // if (req.decoded.email !== email) {
+    //   res.send({ admin: false })
+    // }
+
+    const query = { email: email }
+    const user = await usersCollection.findOne(query);
+    const result = { role: user?.role}
+    res.send(result);
+  })
+
+
+
+
 //   change user role
 app.patch('/users/role/:id', async (req, res) => {
     const id = req.params.id;
