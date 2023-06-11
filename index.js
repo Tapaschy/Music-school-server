46084114f8e1040052425600da0e475cfb3b6788
Tapas chowdhury <tapaschy12@gmail.com>
2023-06-12 01:08:31 +0600

    some changes

--- index.js
+++ index.js
@@ -268,7 +268,6 @@ async function run() {
         
         console.log(payment.classId);
         const deleteResult = await cartCollection.deleteOne(query);
-        // const classId = { _id: new ObjectId(payment.classId) } 
         const enrooledresult = await classesCollection.updateOne(
             { _id: new ObjectId(payment.classId), seats: { $gt: 0 } },
             { $inc: { seats: -1, enrolled: 1 } }
